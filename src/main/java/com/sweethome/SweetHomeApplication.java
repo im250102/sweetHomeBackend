9196acb7b652c756fc188e40c08e89d2c334965a
im250102 <ignacio.martin1083@gmail.com>
2017-11-16 21:25:14 +0100

    first commit

---
+++ src/main/java/com/sweethome/SweetHomeApplication.java
@@ -0,0 +1,19 @@
+package com.sweethome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SweetHomeApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetHomeApplication.class, args);
+	}
+	
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SweetHomeApplication.class);
+	    }
+}
